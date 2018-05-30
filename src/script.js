83debac94bb9db5927f959615efedc28b9c05d93
sagar-savaliya <sagarsavaliya407@gmail.com>
2018-05-31 00:44:39 +0530

    new fetures

--- src/script.js
+++ src/script.js
@@ -1,55 +1,121 @@
-import _ from 'lodash';
-import './style.css';
-const newtodo = document.querySelector('.newtodo');
-const submitbtn = newtodo.querySelector('#submitbtn');
-const list = document.querySelector(".todos");
+// import _ from 'lodash';
+// import './style.css';
+
+const jtodoNew = document.querySelector('.todo__new');
+const jlist = document.querySelector(".todo__list");
+const jall = document.querySelector('.todo__all');
+const jleft = document.querySelector('.todo__left');
+const jclearCompleted = document.querySelector('.todo__completed');
+const jactive = document.querySelector('.todo__active');
+const jfocus = document.querySelector('.focus');
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
-function addNewtodo(e){
-    e.preventDefault();
-    const text = this.querySelector('[name="todo"]').value;
-    const todo = {
-        text: text ,
-        done: false,
-    }
-    if(text){
-        todos.push(todo);
-        populateList();
-        localStorage.setItem('todos',JSON.stringify(todos));
-    }
-    this.reset();
+let mode = "all";
+let leftItemCount = JSON.parse(localStorage.getItem('leftItemCount')) || 0;
+
+jtodoNew.addEventListener('keydown',addNewtodo);
+jlist.addEventListener('click',toggleToDo);
+jlist.addEventListener('click',deleteToDo);
+jlist.addEventListener('dblclick',editToDo);
+jall.addEventListener('click',switchMode);
+jactive.addEventListener('click',switchMode);
+jclearCompleted.addEventListener('click',clearCompleted);
+
+function updateCount() {
+    jleft.innerHTML = `${leftItemCount} ${leftItemCount>1?'items':'item'} left`;
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('todos',JSON.stringify(todos));
+    localStorage.setItem('leftItemCount',JSON.stringify(leftItemCount));
 }
 
 function populateList(){
-    list.innerHTML = todos.map((todo,i) => {
-        return `
-        <li>
-            <input type="checkbox" data-index=${i} id="todo${i}" ${todo.done ? 'checked' : ''}/>
-            <label for="todo${i}"><span class='strike'>${todo.text}</span></label>
-        </li>
-         <span class='delete' data-index=${i}>❌</span>
-        `
+    jlist.innerHTML = todos.map((todo,i) => {
+        var jtodo = jtodo = `
+                       <label class="todo__container" data-index=${i}>
+                            <span class="checkmark">${todo.done ? '✔️' : '⬜'}</span>
+                            <input type="checkbox"><span class="todo__text ${todo.done ? 'strike' : ''}">${todo.text}</span>
+                            <span class='todo__delete' data-index=${i}>☓</span>
+                       </label>
+                      `;
+        if(mode==='all') {
+            return jtodo;
+        }else{
+            return (todo.done ? "" : jtodo);
+        }
     }).reverse().join('');
-    localStorage.setItem('todos',JSON.stringify(todos));
 }
 
-function toggleDone(e){
-    if(e.target.className=='delete')
-    {   
-        deleteTodo(e);
+
+function updateView() {
+    updateCount();
+    populateList();
+    setFocus();
+    updateLocalStorage();
+}
+
+updateView();
+
+
+function clearCompleted(e) {
+    todos = todos.filter(todo => !todo.done);
+    updateView();
+}
+
+function setFocus() {
+    const target = mode=='all'?jall:jactive;
+    jfocus.style.height = `${target.clientHeight}px`;
+    jfocus.style.width = `${target.clientWidth}px`;
+    jfocus.style.top = `${target.offsetTop}px`;
+    jfocus.style.left = `${target.offsetLeft}px`;
+}
+
+function switchMode(e) {
+    mode = e.target.dataset.mode;
+    updateView();
+}
+
+function addNewtodo(e){
+    if(e.key!='Enter')
         return;
+
+    const text = jtodoNew.value;
+    if(!text) return;
+
+    const newtodo = {
+        text: text,
+        done: false,
     }
-    else if(!e.target.matches('input'))
+    leftItemCount++;
+    todos.push(newtodo);
+    updateView();
+    this.value = "";
+}
+
+function toggleToDo(e) {
+    if(e.target.className!='checkmark')
         return;
-    todos[e.target.dataset.index].done = !todos[e.target.dataset.index].done
-    populateList();
+    const todoIndex = e.target.parentElement.dataset.index;
+    todos[todoIndex].done = !todos[todoIndex].done;
+    leftItemCount += todos[todoIndex].done?-1:1;
+    updateView();
 }
 
-function deleteTodo(e){
-    todos.splice(e.target.dataset.index,1);
-    populateList();
+function deleteToDo(e) {
+    if(e.target.className!='todo__delete')
+        return;
+    const todoIndex = e.target.dataset.index;
+    leftItemCount -= todos[todoIndex].done?0:1;
+    todos.splice(todoIndex,1);
+    updateView();
+}
+
+function editToDo(e) {
+
+    if(!e.target.className.includes('todo__text'))
+        return;
+
+    // e.target.contentEditable = 'true';
 }
 
 
-newtodo.addEventListener('submit',addNewtodo);
-list.addEventListener('click',toggleDone);
-populateList();
